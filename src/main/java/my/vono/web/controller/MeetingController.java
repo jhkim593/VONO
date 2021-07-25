f1192bba424ab11659c9868264715d7d8f89464b
jhkim593 <fpdlwjzlr@naver.com>
2021-07-25 10:44:10 +0900

    static ,template

--- src/main/java/my/vono/web/controller/MeetingController.java
+++ src/main/java/my/vono/web/controller/MeetingController.java
@@ -1,5 +1,26 @@
 package my.vono.web.controller;
 
-public class MeetingController {
+import org.springframework.web.bind.annotation.GetMapping;
 
+public class MeetingController {
+	
+	
+	//표지
+//	@GetMapping("/meeting/insert")
+//	public String meetingInsert(){
+//     return "";	
+//}
+	
+	//회의록 목록
+//	@GetMapping("/meetings")
+//	public String meetings(){
+//    return "";	
+//}
+
+	//회의록 작성
+//	@GetMapping("/meeting/write")
+//	public String meetingWrite(){    
+//	return "";	
+	//}
+	
 }
--- src/main/java/my/vono/web/controller/MemberController.java
+++ src/main/java/my/vono/web/controller/MemberController.java
@@ -1,5 +1,28 @@
 package my.vono.web.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class MemberController {
 
+	
+//	@PostMapping("/member/login")
+//	public String memberLogin() {
+//		
+//		return 
+//	} 
+	
+//	@PostMapping("/member/signUp")
+//	public String memberSignUp() {
+//		
+//		return 
+//	} 
+//	@PostMapping("/member/logout")
+//	public String memberLogout() {
+//		return
+//	}
+	
+	
+	
+	
+	
 }
