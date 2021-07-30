8de167191d4e4d0d03a9c9104c92f2a5ca41b626
Juuunew <hyukjuuun@gmail.com>
2021-07-30 17:55:28 +0900

    second commit

--- src/main/java/my/vono/web/controller/MemberController.java
+++ src/main/java/my/vono/web/controller/MemberController.java
@@ -1,22 +1,23 @@
 package my.vono.web.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
-import my.vono.web.model.folder.FolderVO;
-import my.vono.web.model.user.MemberVO;
 import my.vono.web.service.MemberService;
 
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
 	
-	
 	private final MemberService memberService;
+	
+//	@PostMapping("signUpForm")
+//	public String memberSignUp() {
+//		return "signUpForm";
+//	}
 	
 
 	
