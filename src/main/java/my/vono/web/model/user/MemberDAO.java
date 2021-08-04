624cbe92d825251bc4183093988aac6a9258e1d3
Juuunew <hyukjuuun@gmail.com>
2021-08-04 16:53:49 +0900

    signUp ver1

--- src/main/java/my/vono/web/model/user/MemberDAO.java
+++ src/main/java/my/vono/web/model/user/MemberDAO.java
@@ -1,10 +1,25 @@
 package my.vono.web.model.user;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.data.annotation.PersistenceConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import my.vono.web.entity.Member;
 
-public interface MemberDAO extends JpaRepository<Member, Long>{
+@Repository
+public class MemberDAO{
+	
+	@PersistenceContext
+	EntityManager em;
+	
+	public Long save(Member member) {
+		System.out.println("DAO");
 
+		em.persist(member);
+		return member.getId();
+	}
 
 }
--- src/main/java/my/vono/web/service/MemberService.java
+++ src/main/java/my/vono/web/service/MemberService.java
@@ -5,19 +5,23 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
 import my.vono.web.entity.Member;
 import my.vono.web.model.user.MemberDAO;
+import my.vono.web.model.user.MemberVO;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class MemberService {
 	
-	@Autowired
-	MemberDAO memberDAO;
+	private final MemberDAO memberDAO;
 	
 	
-	public void inser(Long a) {
-		memberDAO.findById(a);
+	public Long defaultSignUp(Member member) {
+		System.out.println("서비스");
+
+		return memberDAO.save(member);
 	}
 	
 //	public void inser(Member m) {
