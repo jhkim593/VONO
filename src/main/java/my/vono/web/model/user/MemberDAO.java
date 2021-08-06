01bbc9e139d083c1f3c24d8f8e2017199dd86944
jhkim593 <fpdlwjzlr@naver.com>
2021-08-06 15:48:37 +0900

    mod MemberDAO

--- src/main/java/my/vono/web/model/user/MemberDAO.java
+++ src/main/java/my/vono/web/model/user/MemberDAO.java
@@ -1,11 +1,6 @@
 package my.vono.web.model.user;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import my.vono.web.entity.Member;
 
