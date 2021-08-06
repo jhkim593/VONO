b9945eb3461ab8638bbe6206668f180254999da3
jhkim593 <fpdlwjzlr@naver.com>
2021-08-06 17:55:52 +0900

    Add folderService

--- src/main/java/my/vono/web/model/folder/FolderVO.java
+++ src/main/java/my/vono/web/model/folder/FolderVO.java
@@ -11,6 +11,10 @@ public class FolderVO {
 	
 	private String name;
 	
+	private Long member_id;
+	
+	private Long parent_id;
+	
 	private Boolean is_trash;
 	
 
--- src/main/java/my/vono/web/service/FolderService.java
+++ src/main/java/my/vono/web/service/FolderService.java
@@ -1,14 +1,67 @@
 package my.vono.web.service;
 
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import my.vono.web.entity.Folder;
+import my.vono.web.entity.Member;
+import my.vono.web.exception.FolderNotFoundException;
+import my.vono.web.exception.MemberNotFoundException;
 import my.vono.web.model.folder.FolderDAO;
+import my.vono.web.model.folder.FolderVO;
+import my.vono.web.model.user.MemberDAO;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class FolderService {
 	private final FolderDAO folderDAO;
+	private final MemberDAO memberDAO;
 
+	public void createFolder(FolderVO folderVO) {
+		folderVO.getMember_id();
+		Member member=memberDAO.findById(folderVO.getMember_id()).orElseThrow(MemberNotFoundException::new);
+		
+		Folder parentFolder=null;
+		
+		//확인
+		if(folderVO.getParent_id()!=null) {
+		
+	      parentFolder=folderDAO.findById(folderVO.getParent_id()).orElseThrow(FolderNotFoundException::new);
+		
+		}
 	
+		folderDAO.save(Folder.createFolder(folderVO.getName(),member ,parentFolder));
+		
+		
+	}
+
+	public void deleteFolder(FolderVO folderVO) {
+		
+		Folder folder = folderDAO.findById(folderVO.getId()).orElseThrow(FolderNotFoundException::new);
+		folderDAO.delete(folder);
+
+		
+	}
+
+	public void renameFolder(FolderVO folderVO) {
+
+		Folder folder = folderDAO.findById(folderVO.getId()).orElseThrow(FolderNotFoundException::new);
+		folder.changeFolderName(folderVO.getName());
+
+	}
+
+	public void moveFolder(FolderVO folderVO) {
+//		Folder folder = folderDAO.findById(folderVO.getId()).orElseThrow(FolderNotFoundException::new);
+//		folder.
+		
+
+	}
+
+	public void detailFolder(FolderVO folderVO) {
+
+	}
+
 }
