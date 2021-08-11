3f764f19e61930e2cf20417d54e446ffa120b1bd
jhkim593 <fpdlwjzlr@naver.com>
2021-08-11 11:06:16 +0900

    mod FolderService And FolderController ADD DefaultResponseDto

--- src/main/java/my/vono/web/config/WebSecurityConfig.java
+++ src/main/java/my/vono/web/config/WebSecurityConfig.java
@@ -13,38 +13,38 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	@Override
-	public void configure(WebSecurity web) {
-		web
-		.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-	}
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
-		http
-			.authorizeRequests()
-				.antMatchers("/user/**").authenticated()
-				.anyRequest().permitAll()
-				.and()
-			.formLogin()
-				.loginPage("/home")
-				// form action /login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행해줌
-				.loginProcessingUrl("/login")
-				// 로그인 성공시 돌아가는 url
-				.defaultSuccessUrl("/")
-				.permitAll()
-				.and()
-			.logout()
-				.permitAll();
-	}
-	
-	// 패스워드 암호화
-	// 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-	}
-	
+   
+   @Override
+   public void configure(WebSecurity web) {
+      web
+      .ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+   }
+   
+   @Override
+   protected void configure(HttpSecurity http) throws Exception {
+      http.csrf().disable();
+      http
+         .authorizeRequests()
+            .antMatchers("/user/**").authenticated()
+            .anyRequest().permitAll()
+            .and()
+         .formLogin()
+            .loginPage("/home")
+            // form action /login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행해줌
+            .loginProcessingUrl("/login")
+            // 로그인 성공시 돌아가는 url
+            .defaultSuccessUrl("/")
+            .permitAll()
+            .and()
+         .logout()
+            .permitAll();
+   }
+   
+   // 패스워드 암호화
+   // 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다
+   @Bean
+   public PasswordEncoder passwordEncoder() {
+      return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+   }
+   
 }
