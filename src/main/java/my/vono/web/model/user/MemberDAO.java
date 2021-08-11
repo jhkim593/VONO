31aa9f9c53b60af4af47192b071b0a56889e8c5e
Juuunew <hyukjuuun@gmail.com>
2021-08-11 13:29:50 +0900

    modify WebSecurityConfig

--- src/main/java/my/vono/web/model/user/MemberDAO.java
+++ src/main/java/my/vono/web/model/user/MemberDAO.java
@@ -12,7 +12,5 @@ public interface MemberDAO extends JpaRepository<Member,Long>{
 //	@Query("select m from Member m where m.=:lo")
 //	Optional<Member>findByMemberWithLoginIdAndPw(@Param("login_id")String login_id,@Param("pw")String pw);
 //	
-
-	Optional<Member> findById(Long id);
 }
 
