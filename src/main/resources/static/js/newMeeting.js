80acf00a899487421e39a1f5af3e4ed86cda473c
NookeryLim <dlasnrk@naver.com>
2021-08-19 09:35:24 +0900

    GSTT6

--- src/main/resources/static/js/newMeeting.js
+++ src/main/resources/static/js/newMeeting.js
@@ -1,19 +1,15 @@
-/*$("#meeting_start").click(function(){
-	const name=$("#mt_name").val();
-	const date=$("#mt_date").val();
-	const participant=$("#mt_participant").val();
-	const content=$("#mt_content").val();
-	const reference=$("#mt_reference").val();
-	alert("content : "+content);
-	$.get('startMeeting',
-		{name,
-		date,
-		participant,
-		content,
-		reference},
-		function(data,status){
-			alert(data);
-		}
-		
-	);
-});*/
+/*function updateSpeak() {
+    var speakBean = {
+        time:$("#time").val(),
+        speaker: $("#speaker").val(),
+        transcript: $("#transcript").val()
+    };
+    $.ajax({
+        url: "/updateSpeak",
+        type: "POST",
+        data: speakBean,
+    })
+    .done(function (fragment) {
+        $('#speakTable').replaceWith(fragment);
+    });
+}*/
