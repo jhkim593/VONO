72186f3b9d6e17dbc705e7fcb49c880132218795
NookeryLim <83194150+NookeryLim@users.noreply.github.com>
2021-08-22 13:25:50 +0900

    Update WebSecurityConfig.java

--- src/main/java/my/vono/web/config/WebSecurityConfig.java
+++ src/main/java/my/vono/web/config/WebSecurityConfig.java
@@ -38,8 +38,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
       http.csrf().disable();
       http
          .authorizeRequests()
-            .antMatchers("/user/**", "/newMeeting/**", "/wasteBasket/**").authenticated()
-            //.anyRequest().permitAll()
+            .antMatchers("/user/**").authenticated()
+            .anyRequest().permitAll()
             .and()
          .formLogin()
             .loginPage("/")
