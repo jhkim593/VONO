79823a66d2f7072cd8da615dbd8d0424c382fb22
gang-ss <hi.gangss@gmail.com>
2021-08-22 14:06:22 +0900

    WB cm3

--- src/main/java/my/vono/web/model/file/RecFileDAO.java
+++
@@ -1,9 +0,0 @@
-package my.vono.web.model.file;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import my.vono.web.entity.RecFile;
-
-public interface RecFileDAO extends JpaRepository<RecFile, Long>{
-
-}
